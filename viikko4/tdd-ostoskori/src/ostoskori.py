aac5da34cb38d65c8e91d0d3ef26980cf1bdadd4
oskari83 <oskpelto@gmail.com>
2022-02-11 20:39:23 +0200

    tdd 2 ostoksen jälkeen korissa 1 olio

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -6,6 +6,7 @@ class Ostoskori:
         self._hinta = 0
         self._amountKorissa = 0
         self._kori = []
+        self._lisatytnimet = []
         # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
 
     def tavaroita_korissa(self):
@@ -17,11 +18,13 @@ class Ostoskori:
     def lisaa_tuote(self, lisattava: Tuote):
         prod = Ostos(lisattava)
         self._amountKorissa +=1
-        if prod in self._kori:
-            i = self._kori.index(prod)
-            self._kori[i].muuta_lukumaaraa(1)
+        if prod.tuotteen_nimi() in self._lisatytnimet:
+            for i in range(len(self._kori)):
+                if self._kori[i].tuotteen_nimi()==prod.tuotteen_nimi:
+                    self._kori[i].muuta_lukumaaraa(1)
         else:
             self._kori.append(prod)
+            self._lisatytnimet.append(prod.tuotteen_nimi())
         self._hinta += lisattava.hinta()
 
     def poista_tuote(self, poistettava: Tuote):
